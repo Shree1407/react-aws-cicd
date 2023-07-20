fd3e30083cfd01277ec10950f6dcf205462ec19b
Shreekant Kolekar <Shreekant.Kolekar@ltimindtree.com>
2023-07-20 13:10:10 +0530

    Check commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Added new puch
+          Edit <code>src/App.js</code> and save to reload. new push
         </p>
         <a
           className="App-link"
